3df1eb71e17c4dec7b17883e5fbf25145025409d
Emre <eaydin@verdazo.com>
2020-04-05 14:42:24 +0300

    Solved two 900 Questions

---
+++ 339A_HelpfulMaths/339A_HelpfulMaths.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string s; cin >> s;
+	s.erase(remove(s.begin(), s.end(), '+'), s.end());
+	sort(s.begin(), s.end());
+
+	for(auto i = 0; i != s.size() - 1; i+=2)
+	{
+		s.insert(i+1, "+");
+	}
+	cout << s;
+}
